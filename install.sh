766a8d38fbc03b50b55029f497d4f1f2812d7de6
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-02-11 16:29:41 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ bash <(curl -s "https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/
 source .bash_profile
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install curl git wget htop tmux build-essential jq make gcc -y
 
 CELESTIA_PORT=11
 echo "export WALLET="ERN" >> $HOME/.bash_profile
@@ -13,21 +12,6 @@ echo "export MONIKER="ERN" >> $HOME/.bash_profile
 echo "export CHAIN_ID="mocha-4"" >> $HOME/.bash_profile
 echo "export CELESTIA_PORT="${CELESTIA_PORT}"" >> $HOME/.bash_profile
 source $HOME/.bash_profile
-
-
-cd ~
-! [ -x "$(command -v go)" ] && {
-VER="1.21.3"
-wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
-rm "go$VER.linux-amd64.tar.gz"
-[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
-echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
-source ~/.bash_profile
-}
-[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
-
 
 cd $HOME 
 rm -rf celestia-app 
