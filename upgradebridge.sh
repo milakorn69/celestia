1e1ebc4b339c2f784c322754609b5b14d15e3299
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-04-22 23:16:45 +0500

    Update upgradebridge.sh

--- upgradebridge.sh
+++ upgradebridge.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+sudo systemctl stop celestia-bridge
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
 cd celestia-node/
 git checkout tags/v0.13.4 
 make build 
+sudo make install 
+make cel-key 
+celestia bridge config-update --p2p.network mocha
 systemctl daemon-reload
 systemctl restart celestia-bridge && journalctl -u celestia-bridge -f -o cat
