a0b2aa12dcba5f0e3cc25a60ea1a34b02c68b031
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-07-07 16:23:02 +0500

    Update installfulltest.sh

--- installfulltest.sh
+++ installfulltest.sh
@@ -48,7 +48,7 @@ cd $HOME
 sudo apt install lz4 -y
 rm -rf ~/.celestia-app/data
 mkdir -p ~/.celestia-app/data
-wget -O snap_celestia.tar.lz4 https://testnets.services-ernventures.com/celestia/snap_celestia.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.celestia-app/data
+wget -O snap_celestia.tar.lz4 https://testnets.services-ernventures.com/celestia/snap_celestia.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.celestia-app/
 
 sudo tee /etc/systemd/system/celestia-appd.service > /dev/null <<EOF
 [Unit]
