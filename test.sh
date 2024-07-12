630d45ba021eec47a58d752c6544c2728baf5fc4
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-07-12 11:52:37 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -15,6 +15,7 @@ NODE_IPS=(
 RPC_PORT="26657"
 
 # Получаем список валидаторов
+echo "Получаем список валидаторов..."
 validators=$(celestia-appd q staking validators --output json)
 
 # Проверка, удалось ли получить список валидаторов
@@ -23,16 +24,31 @@ if [[ -z "$validators" ]]; then
     exit 1
 fi
 
+echo "Список валидаторов получен. Данные валидаторов:"
+echo "$validators" | jq .
+
 # Инициализация файла для пиров
 > peers.txt
 
 # Получаем информацию о пирах с нескольких узлов и портов
 for NODE_IP in "${NODE_IPS[@]}"; do
+    echo "Запрашиваем информацию у узла $NODE_IP на порту $RPC_PORT..."
     response=$(curl -s http://$NODE_IP:$RPC_PORT/net_info)
+    
+    # Выводим ответ для отладки
+    echo "Ответ от узла:"
+    echo "$response" | jq .
+    
     if echo "$response" | jq empty &> /dev/null; then
         echo "$response" | jq -r '.result.peers[] | .node_info.moniker + " " + .remote_ip' >> peers.txt
+    else
+        echo "Нет данных от узла $NODE_IP:$RPC_PORT"
     fi
 done
+
+# Проверка содержимого файла peers.txt
+echo "Содержимое файла peers.txt:"
+cat peers.txt
 
 # Парсинг JSON и сопоставление валидаторов и IP-адресов
 echo "$validators" | jq -r '.validators[] | .description.moniker + " " + .operator_address' > validators.txt
@@ -44,5 +60,7 @@ while read -r validator; do
     ip_list=$(grep "$moniker" peers.txt | awk '{print $2}' | sort | uniq)
     if [[ -n "$ip_list" ]]; then
         echo "Валидатор: $moniker, Адрес: $address, IP: $ip_list"
+    else
+        echo "Для валидатора $moniker не найдено IP-адресов."
     fi
 done < validators.txt
