1532d6f5ed3bde6a47beeb63501a82be852afb6c
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-07-12 09:45:35 +0500

    Update validatorlist.sh

---
+++ validatorlist.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+NODE_IP="136.243.176.86"
+RPC_PORT="26657"
+
+# Получаем список валидаторов
+validators=$(celestia-appd q staking validators --output json)
+
+# Получаем информацию о пирах
+peers=$(curl -s http://$NODE_IP:$RPC_PORT/net_info)
+
+# Проверка, удалось ли получить список валидаторов и пиров
+if [[ -z "$validators" ]]; then
+    echo "Не удалось получить список валидаторов"
+    exit 1
+fi
+
+if [[ -z "$peers" ]]; then
+    echo "Не удалось получить информацию о пирах"
+    exit 1
+fi
+
+# Парсинг JSON и сопоставление валидаторов и IP-адресов
+echo "$validators" | jq -r '.validators[] | .description.moniker + " " + .operator_address' > validators.txt
+echo "$peers" | jq -r '.result.peers[] | .node_info.moniker + " " + .remote_ip' > peers.txt
+
+echo "Список валидаторов и их IP-адресов:"
+while read -r validator; do
+    moniker=$(echo "$validator" | awk '{print $1}')
+    address=$(echo "$validator" | awk '{print $2}')
+    ip=$(grep "$moniker" peers.txt | awk '{print $2}')
+    echo "Валидатор: $moniker, Адрес: $address, IP: $ip"
+done < validators.txt
