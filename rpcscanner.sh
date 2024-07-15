b40439dfad9797a8c7f609963235fa4bf7b740a6
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-07-15 15:25:06 +0500

    Update rpcscanner.sh

--- rpcscanner.sh
+++ rpcscanner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Задаем список возможных RPC портов
-RPC_PORTS=(26657 26658 26659 26660)
+RPC_PORTS=(26657 26658 26659 26660 26667 21657 27657 35657 43657 26698 34657 26647 18657 11657 443)
 
 # Файл для записи результатов
 OUTPUT_FILE="rpc_endpoints.txt"
@@ -9,13 +9,22 @@ OUTPUT_FILE="rpc_endpoints.txt"
 # Очищаем файл перед записью
 > $OUTPUT_FILE
 
+# Функция для проверки доступности RPC на заданном порту
+check_rpc() {
+  local ip=$1
+  local port=$2
+  if curl -s --head "http://$ip:$port/status" | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null; then
+    echo "http://$ip:$port/" >> $OUTPUT_FILE
+  fi
+}
+
 # Выполняем запрос к локальному RPC серверу и извлекаем адреса узлов
 PEER_IPS=$(curl -s http://localhost:26657/net_info | jq -r '.result.peers[].remote_ip')
 
-# Записываем каждый IP на каждом из возможных портов
+# Проверяем каждый IP на каждом из возможных портов
 for ip in $PEER_IPS; do
   for port in "${RPC_PORTS[@]}"; do
-    echo "http://$ip:$port/" >> $OUTPUT_FILE
+    check_rpc $ip $port
   done
 done
 
