cc9c46d22ac3337ad76b61e1f4282dd11ec06a87
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-09-17 10:23:06 +0500

    Update upgradenode.sh

--- upgradenode.sh
+++ upgradenode.sh
@@ -3,7 +3,7 @@ sudo systemctl stop celestia-bridge
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
 cd celestia-node/
-git checkout tags/v0.16.0-rc0
+git checkout tags/v0.16.0
 make build 
 sudo make install 
 make cel-key 
